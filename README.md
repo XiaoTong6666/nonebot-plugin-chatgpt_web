5389df30cd80a057e3cf2e4cc0ba179eed814f0e
XiaoTong6666 <3278671549@qq.com>
2025-10-19 12:40:12 +0800

    ”优化部署流程“

--- README.md
+++ README.md
@@ -6,9 +6,10 @@ git clone https://github.com/XiaoTong6666/nonebot-plugin-chatgpt_web
 ```
 需要使用Cookie-Editor获取浏览器cookie的json放到`data/zhanghao_cookies.json`,根据实际情况修改配置文件`.env.prod`（包括代理配置，连接onebot配置）
 ```
-apt install xvfb chromium fonts-wqy-zenhei 
+apt install chromium fonts-wqy-zenhei 
 # 如果是无头环境还需要
-apt install tigervnc-standalone-server tigervnc-tools x11vnc openbox
+apt install xvfb x11vnc openbox # 无头组合1
+apt install tigervnc-standalone-server tigervnc-tools # 无头组合2
 pip install -r requirements.txt
 pipx install nb-cli
 nb adapter install nonebot-adapter-onebot
@@ -20,6 +21,7 @@ nb adapter install nonebot-adapter-onebot
 4. run your bot using `nb run --reload` .
 ## Docker
 ```
+docker pull xiaotong666/gpt-bot:latest
 docker run -d --name gpt-bot -p 5789:5789 -p 5910:5901 --add-host=host.docker.internal:host-gateway -v "$(pwd)/.env.prod:/app/.env.prod" -v "$(pwd)/data:/app/data" gpt-bot
 ```
 
--- start.sh
+++ start.sh
@@ -11,24 +11,24 @@ if [ -n "$DISPLAY" ]; then
     exit 0
 fi
 if [ "$in_docker" = "1" ]; then
-    n="0"
+    n="1"
     echo "检测到Docker环境了喵~"
 else
     while true; do
         echo "没有DISPLAY喵，将使用无头模式喵！"
         echo "请选择启动模式："
-        echo "0) Xvfb + x11vnc + NoneBot"
-        echo "1) Tigervnc + NoneBot"
-        read -p "0 或 1 喵: " n
+        echo "1) Xvfb + x11vnc + NoneBot"
+        echo "2) Tigervnc + NoneBot"
+        read -p "1 或 2 喵: " n
 
-        if [ "$n" = "0" ] || [ "$n" = "1" ]; then
+        if [ "$n" = "1" ] || [ "$n" = "2" ]; then
             break
         else
-            echo "你再干嘛喵？请重新选择喵~"
+            echo "你在干嘛喵？请重新选择喵~"
         fi
     done
 fi
-if [ "$n" = "0" ]; then
+if [ "$n" = "1" ]; then
     Xvfb :1 -screen 0 1280x720x16 &
     export DISPLAY=:1
     sleep 2
@@ -37,7 +37,7 @@ if [ "$n" = "0" ]; then
     openbox &
     echo "正在启动NoneBot喵ing..."
     $HOME/.local/bin/nb run
-elif [ "$n" = "1" ]; then
+elif [ "$n" = "2" ]; then
     vncserver :1 -localhost no -geometry 1280x720 -depth 16
     export DISPLAY=:1
     sleep 1
