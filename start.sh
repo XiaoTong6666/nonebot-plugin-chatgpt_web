6e7b8d910fac7ac1ae9d8f0abf96ded6eddca228
XiaoTong6666 <3278671549@qq.com>
2025-10-31 20:08:32 +0800

    docker

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ if [ -n "$DISPLAY" ]; then
     exit 0
 fi
 if [ "$in_docker" = "1" ]; then
-    n="1"
+    n="2"
     echo "检测到Docker环境了喵~"
 else
     while true; do
@@ -29,16 +29,16 @@ else
     done
 fi
 if [ "$n" = "1" ]; then
-    Xvfb :1 -screen 0 1280x720x16 &
+    Xvfb :1 -screen 0 1280x720x24 &
     export DISPLAY=:1
     sleep 2
-    x11vnc -rfbport 5901 -display :1 -rfbauth /root/.vnc/passwd -forever -bg -nowf
+    x11vnc -rfbport 5910 -display :1 -rfbauth /root/.vnc/passwd -forever -listen 0.0.0.0 &
     sleep 1
     openbox &
     echo "正在启动NoneBot喵ing..."
     $HOME/.local/bin/nb run
 elif [ "$n" = "2" ]; then
-    vncserver :1 -localhost no -geometry 1280x720 -depth 16
+    vncserver :1 -localhost no -geometry 1280x720 -depth 24
     export DISPLAY=:1
     sleep 1
     openbox &
